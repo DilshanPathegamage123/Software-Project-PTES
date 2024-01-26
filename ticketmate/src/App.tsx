37b786a2d7dc897bdc301823e1c90c349a08d9b0
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-01-26 09:11:32 -0800

    buttons

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import PrimaryButton from './Components/Buttons/PrimaryButton'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <PrimaryButton type="button" value="Click Me" color="primary" IsSmall={true}/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="secondary"/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="third" width="400px"/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="yellow" width="400px"/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="primary" IsBlock={true}/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="secondary" IsBlock={true}/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="third" IsBlock={true}/>
+      <br />
+      <PrimaryButton type="button" value="Click Me" color="yellow" IsBlock={true}/>
     </>
   )
 }

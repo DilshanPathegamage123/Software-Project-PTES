ddb8fad7e2d90570def800781f2a31ac60ac8982
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-01-27 11:19:08 +0530

    Initial Commit
    1) Home page branch is created.
    2) Home Page Started.

---
+++ ticketmate/src/Pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+import NavBar1 from '../Components/NavigationBar/NavBar1'
+
+const Home = () => {
+  return (
+    <div>
+     <NavBar1 /> 
+    </div>
+  )
+}
+
+export default Home

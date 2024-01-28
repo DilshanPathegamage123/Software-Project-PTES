f0f5a7df7a2b5587aa04ce1822d74ec884c557dd
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-01-28 06:02:08 -0800

    NavBar

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,27 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import PrimaryButton from './Components/Buttons/PrimaryButton'
+import PrimaryNavBar from './Components/NavBar/PrimaryNavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PrimaryButton type="button" value="Click Me" color="primary" IsSmall={true}/>
+      <PrimaryNavBar/>
       <br />
-      <PrimaryButton type="button" value="Click Me" color="secondary"/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="third" width="400px"/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="yellow" width="400px"/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="primary" IsBlock={true}/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="secondary" IsBlock={true}/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="third" IsBlock={true}/>
-      <br />
-      <PrimaryButton type="button" value="Click Me" color="yellow" IsBlock={true}/>
     </>
   )
 }

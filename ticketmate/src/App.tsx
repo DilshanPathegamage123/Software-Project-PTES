7be9bdedc0ef3c2f37c87f8c285466546cb98feb
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-01-29 17:25:31 -0800

    NavBar (#9)
    * Main (#3)
    
    * initial commit
    
    * update.gitignore
    
    * all variables
    
    * all variables
    
    ---------
    
    Co-authored-by: Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
    
    * buttons
    
    * NavBar
    
    ---------
    
    Co-authored-by: Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <button type="button" className="btn btn-primary">Primary</button>
-    <input className="btn btn-primary" type="reset" value="Reset"></input>
+
       
+
     </>
   )
 }

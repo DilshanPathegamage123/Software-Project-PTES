a889c1292acdf4974dfcdaf8a64335aa53ad1e24
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-02-01 09:15:49 -0800

    Bus Owner page and Bus Registration Page

--- ticketmate/src/Components/Buttons/PrimaryButton.tsx
+++ ticketmate/src/Components/Buttons/PrimaryButton.tsx
@@ -24,10 +24,14 @@ function PrimaryButton(props: any) {
       width: props.width,
     };
 
+    const LINK = () => {
+      window.location.href = props.link;
+    }
+
     return (
       <>
           <div>
-              <input type={props.type} className={`btn btn-primary ${classColor} ${Block} ${size}`} value={props.value}  style={buttonStyles}/>
+              <input type={props.type} className={`btn btn-primary ${classColor} ${Block} ${size}`} value={props.value}  style={buttonStyles} onClick={LINK}/>
           </div>
       </>
   )
---
+++ ticketmate/src/Pages/BusOwnerPage/BusOwnerPage.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PrimaryNavBar from '../../Components/NavBar/PrimaryNavBar'
+import ProfileSection from '../../Components/ProfileSection/ProfileSection'
+import SquareButton from '../../Components/Buttons/SquareButton/SquareButton'
+import './BusOwnerPage.css'
+
+function BusOwnerPage() {
+  return (
+    <>
+        <PrimaryNavBar/>
+        <div className='container-fluid pt-3'>
+          <div>
+            <ProfileSection/>
+          </div>
+          <div className='row'>
+            <div className='col-lg-2'>
+              <div>
+                <SquareButton text="Register a new Bus" link="/BusRegistrationPage"/>
+              </div>
+              <div>
+                <SquareButton text="Create a new travel session" link="/BusRegistrationPage"/>
+              </div>  
+            </div>
+
+            <div className='col-lg-10 rounded-4'>
+             
+            </div>
+          </div>
+          
+            
+        </div>
+        
+        
+    </>
+  )
+}
+
+export default BusOwnerPage

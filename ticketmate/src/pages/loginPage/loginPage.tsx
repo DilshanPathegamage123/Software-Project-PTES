53078d22e93dce569cbe12f8a165bee2e407a1e4
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-02-02 11:27:25 +0530

    folder structure added

--- ticketmate/src/pages/loginPage/loginPage.tsx
+++ ticketmate/src/pages/loginPage/loginPage.tsx
@@ -1,4 +1,4 @@
-import PrimaryNavBar from "../Components/NavBar/PrimaryNavBar";
+import PrimaryNavBar from "../../Components/NavBar/PrimaryNavBar";
 import React from 'react';
 
 function LoginPage() {

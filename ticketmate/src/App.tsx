8746af65a44840826e344b22a7ea72efcfd02f8e
Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
2024-02-13 19:13:54 +0530

    login page
    the login page is done

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
-import LoginPage from './pages/loginPage/loginPage'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginPage />
-    
+      
+ 
+      
 
     </>
   )

83e4b8bd5541e14cb43b843885ac76564a339af4
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-04-08 06:30:32 -0800

    Bus registration (#21)
    * Main (#3)
    
    * initial commit
    
    * update.gitignore
    
    * all variables
    
    * all variables
    
    ---------
    
    Co-authored-by: Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>
    
    * buttons
    
    * NavBar
    
    * Bus Owner page and Bus Registration Page
    
    * added nav bar to Bus owner page and Bus registration page
    
    * test commit
    
    * this is for correct issue
    
    * this is a correction of the issue
    
    * Revert "added nav bar to Bus owner page and Bus registration page"
    
    This reverts commit c5085113a913d6d99cda28dfe58b47606a610e5b.
    
    * solved the error
    
    * bue owner page updates
    
    * updated file but there is an error
    
    * retrive data from the database is okay. but send to the database is not finalised.
    
    * there is an error in the responsiveness of the code.
    
    * Revert "retrive data from the database is okay. but send to the database is not finalised."
    
    This reverts commit f1e7b1f3115ce8453e4e2af47fcc03405ca6377a.
    
    * data retrive from database okay. after formating the folders.
    
    * retrive the data from database ok. after formating the folder structure.
    
    * form validation ok.
    
    * form validation is ok 2
    
    * form value submit ok without images
    
    ---------
    
    Co-authored-by: Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,9 +1,17 @@
+
 import React, { useState } from 'react'
+
 
 function App() {
   return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path='/' element={<BusOwnerPage/>}></Route>
+    //     <Route path='/BusRegistrationPage' element={<BusRegistrationPage/>}></Route>
+    //     <Route path='/CreateBusJourneyPage' element={<CreateBusJourneyPage/>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
     <>
-
 
     </>
   );

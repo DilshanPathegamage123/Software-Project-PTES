32e9fa9e9be3e59f0ee7b83473296004aba9c6d8
DilshanPathegamage123 <141545307+DilshanPathegamage123@users.noreply.github.com>
2024-04-09 15:51:08 +0530

    Travel options page (#18)
    * Initial Commit
    
    * Api calls are added and some validations are added
    
    * Api Calls and Some validations added
    
    * Alert created
    
    * Modification of the vehicle type selection is added
    
    Coding part is done for send selected vehicle type in the home page when searching is send to travel option page for modify search. For the modification, vehicle type is already selected from the home page(TotalBlock component)
    
    * New api links added.
    
    * Errors fixed and some validations are added  in the location selectors.
    
    * Display travel options after search - Big part done
    
    * Travel options display after search part is created.
    
    * Search from Home page and search from travel options page is done. React Toastify used for alerts.
    
    * Seat button is created.
    
    * Seat structure component is created.
    
    * Display Search Results Is added 2.
    
    ---------
    
    Co-authored-by: Shanuka Lakshan <121119338+SILVA-HSL@users.noreply.github.com>

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -1,17 +1,17 @@
+
 import React from "react";
 import AppRoutes from "./Routes";
 import React, { useState } from 'react'
 
-
-
 function App() {
+
   return (
+
     <div className="App" id="app">
       <AppRoutes/>
       {/* <AdminPage/> */}
     </div>
       
-
   );
 }
 

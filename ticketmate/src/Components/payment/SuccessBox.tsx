f80d349a0c1f26749652ebff6d48757596017390
Himasha Sandeepani <141569662+SANDEEPANI-RMH@users.noreply.github.com>
2024-04-13 12:57:54 +0530

    Passenger Profile created
    add e mail sending part
    
    passenger profile (booking, travel history, notification)

--- ticketmate/src/Components/payment/SuccessBox.tsx
+++ ticketmate/src/Components/payment/SuccessBox.tsx
@@ -1,14 +1,17 @@
 import Rec2 from "./asset/rec2.png";
 import Checkmark from "./asset/checkmark.png";
+// import { Link } from "react-router-dom";
+
 
 function SuccessBox() {
-    const buttonStyle = {
-        backgroundColor: 'rgb(4,47,64)',
-        color: 'white', // Optionally change text color to ensure readability
-        
-        width: '75%',
-        height: '60%',
-      };
+  const buttonStyle = {
+    backgroundColor: "rgb(4,47,64)",
+    color: "white", // Optionally change text color to ensure readability
+
+    width: "75%",
+    height: "60%",
+  };
+  //  const usenavigate = useNavigate();
   return (
     <>
       {/* <div className="container">
@@ -46,11 +49,24 @@ function SuccessBox() {
               Thank You
             </p>
 
-            
-              <button type="button" className="btn btn-primary ml-3 " style={buttonStyle}>
-                Receipt
-              </button>
-            
+            {/* <button
+              type="button"
+              className="btn btn-primary ml-3"
+              style={buttonStyle}
+              onClick={() => window.location.replace('/payment4}/')}
+            >
+              Receipt
+            </button> */}
+          {/* <Link to="/payment4"> */}
+            <button
+              type="button"
+              className="btn btn-primary ml-3"
+              style={buttonStyle}
+               // Remove the argument from the useNavigate function call
+            >
+              Receipt
+            </button>
+          {/* </Link> */}
           </div>
         </div>
       </div>
--- ticketmate/src/pages/loginPage/paymentmain.tsx
+++ ticketmate/src/pages/loginPage/paymentmain.tsx
@@ -23,10 +23,7 @@ function Payment(){
             <div className="row">
               <Selection/>
             </div>
-            {/* <div className="row">
-              <Payment1/>
-            </div> */}
-          
+           
            
    
    

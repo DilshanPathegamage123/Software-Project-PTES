dd8c530a3c8cc3ccff4bef4ac529f7ce66d52fc6
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-05-29 09:35:56 -0800

    filter according to user id (register buses and scheduled buses)

--- ticketmate/src/App.tsx
+++ ticketmate/src/App.tsx
@@ -3,11 +3,11 @@ import React, { useState } from 'react'
 import BusRegistrationPage from './Pages/BusRegistrationPage/BusRegistrationPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import BusOwnerPage from './Pages/BusOwnerPage/BusOwnerPage';
-import CreateBusJourneyPage from './Pages/CreateBusJourneyPage/CreateBusJourneyPage';
 import RegisteredBusPage from './Pages/RegisteredBusPage/RegisteredBusPage';
 import ScheduledBusPage from './Pages/ScheduledBusPage/ScheduledBusPage';
 import BusSchedulePage from './Pages/BusSchedulePage/BusSchedulePage';
 import LoginPage from './Pages/loginPage/loginPage';
+import Home from './Pages/HomePage/Home';
 
 
 function App() {
@@ -15,18 +15,19 @@ function App() {
     <BrowserRouter>
       <Routes>
         
-        <Route path='/' element={<BusOwnerPage/>}></Route>
+        <Route path='/' element={<Home/>}></Route> 
+        <Route path='/LoginPage' element={<LoginPage/>}></Route>
+        <Route path='/BusOwnerPage' element={<BusOwnerPage/>}></Route>
         <Route path='/BusRegistrationPage' element={<BusRegistrationPage/>}></Route>
-        <Route path='/CreateBusJourneyPage' element={<CreateBusJourneyPage/>}></Route>
         <Route path='/RegisteredBusPage' element={<RegisteredBusPage/>}></Route>
         <Route path='/ScheduledBusPage' element={<ScheduledBusPage/>}></Route>
         <Route path='/BusSchedulePage' element={<BusSchedulePage/>}></Route>
-        <Route path='/LoginPage' element={<LoginPage/>}></Route>
+        
 
       </Routes>
     </BrowserRouter>
     // <>
-    //   <BusOwnerPage/>
+    //   <BusSchedulePage/>
     // </>
   );
 }
--- ticketmate/src/main.tsx
+++ ticketmate/src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.tsx'
-import './index.css'
-
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
- 
-)
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>,
+    rootElement
+  );
+}
 

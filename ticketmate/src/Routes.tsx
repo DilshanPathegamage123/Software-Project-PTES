714b2926ecfd330f16969e5b1e1cd9e9f25dcbfe
Nipun Malinga Premalal <nipunmalinga5@gmail.com>
2024-06-18 11:21:43 -0800

    Routes add to Routes component

--- ticketmate/src/Routes.tsx
+++ ticketmate/src/Routes.tsx
@@ -26,47 +26,39 @@ import TrainShceduleUpdatePage2 from './Pages/TrainShceduleUpdatePage/TrainShced
 import TrainShceduleUpdatePage3 from './Pages/TrainShceduleUpdatePage/TrainShceduleUpdatePage3';
 import TrainShceduleUpdatePage4 from './Pages/TrainShceduleUpdatePage/TrainShceduleUpdatePage4';
 import TrainShceduleUpdatePage5 from './Pages/TrainShceduleUpdatePage/TrainShceduleUpdatePage5';
-import AppRoutes from './Routes';
 
-
-
-function App() {
+function AppRoutes() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Home />} />
-    //     <Route path='/LoginPage' element={<LoginPage />} />
-    //     <Route path='/BusOwnerPage' element={<BusOwnerPage />} />
-    //     <Route path='/BusRegistrationPage' element={<BusRegistrationPage />} />
-    //     <Route path='/RegisteredBusPage' element={<RegisteredBusPage />} />
-    //     <Route path='/ScheduledBusPage' element={<ScheduledBusPage />} />
-    //     <Route path='/BusSchedulePage' element={<BusSchedulePage />} />
-    //     <Route path='/TrainOwnerPage' element={<TrainOwnerPage />} />
-    //     <Route path='/TrainRegistrationPage' element={<TrainRegistrationPage />} />
-    //     <Route path='RegisteredLocomitivePage' element={<RegisteredLocomitivePage />}/>
-    //     <Route path='/RegisteredCarriagePage' element={<RegisteredCarriagePage />} />
-    //     <Route path='/ScheduledTrainPage' element={<ScheduledTrainPage />} />
-    //     <Route path='/TrainSchedulePage' element={<TrainSchedulePage />} />
-    //     <Route path='/UpdateBusRegInfoPage' element={<UpdateBusRegInfoPage />} />
-    //     <Route path='/BusScheduleFormUpdatePage' element={<BusScheduleFormUpdatePage />} />
-    //     <Route path='/BusScheduleFormUpdatePage2' element={<BusScheduleFormUpdatePage2/>} />
-    //     <Route path='/BusScheduleFormUpdatePage3' element={<BusScheduleFormUpdatePage3 />} />
-    //     <Route path='/UpdateCarriageRegInfoPage' element={<UpdateCarriageRegInfoPage />} />
-    //     <Route path='/UpdateLocomotiveRegInfoPage' element={<UpdateLocomotiveRegInfoPage />} />
-    //     <Route path='/TrainShceduleUpdatePage' element={<TrainShceduleUpdatePage />} />
-    //     <Route path='/TrainShceduleUpdatePage2' element={<TrainShceduleUpdatePage2 />} />
-    //     <Route path='/TrainShceduleUpdatePage3' element={<TrainShceduleUpdatePage3 />} />
-    //     <Route path='/TrainShceduleUpdatePage4' element={<TrainShceduleUpdatePage4 />} />
-    //     <Route path='/TrainShceduleUpdatePage5' element={<TrainShceduleUpdatePage5 />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/LoginPage' element={<LoginPage />} />
+        <Route path='/BusOwnerPage' element={<BusOwnerPage />} />
+        <Route path='/BusRegistrationPage' element={<BusRegistrationPage />} />
+        <Route path='/RegisteredBusPage' element={<RegisteredBusPage />} />
+        <Route path='/ScheduledBusPage' element={<ScheduledBusPage />} />
+        <Route path='/BusSchedulePage' element={<BusSchedulePage />} />
+        <Route path='/TrainOwnerPage' element={<TrainOwnerPage />} />
+        <Route path='/TrainRegistrationPage' element={<TrainRegistrationPage />} />
+        <Route path='RegisteredLocomitivePage' element={<RegisteredLocomitivePage />}/>
+        <Route path='/RegisteredCarriagePage' element={<RegisteredCarriagePage />} />
+        <Route path='/ScheduledTrainPage' element={<ScheduledTrainPage />} />
+        <Route path='/TrainSchedulePage' element={<TrainSchedulePage />} />
+        <Route path='/UpdateBusRegInfoPage' element={<UpdateBusRegInfoPage />} />
+        <Route path='/BusScheduleFormUpdatePage' element={<BusScheduleFormUpdatePage />} />
+        <Route path='/BusScheduleFormUpdatePage2' element={<BusScheduleFormUpdatePage2/>} />
+        <Route path='/BusScheduleFormUpdatePage3' element={<BusScheduleFormUpdatePage3 />} />
+        <Route path='/UpdateCarriageRegInfoPage' element={<UpdateCarriageRegInfoPage />} />
+        <Route path='/UpdateLocomotiveRegInfoPage' element={<UpdateLocomotiveRegInfoPage />} />
+        <Route path='/TrainShceduleUpdatePage' element={<TrainShceduleUpdatePage />} />
+        <Route path='/TrainShceduleUpdatePage2' element={<TrainShceduleUpdatePage2 />} />
+        <Route path='/TrainShceduleUpdatePage3' element={<TrainShceduleUpdatePage3 />} />
+        <Route path='/TrainShceduleUpdatePage4' element={<TrainShceduleUpdatePage4 />} />
+        <Route path='/TrainShceduleUpdatePage5' element={<TrainShceduleUpdatePage5 />} />
         
-    //   </Routes>
-    // </BrowserRouter>
-
-    <AppRoutes/>
-        // <>
-        //   <BusScheduleFormUpdatePage/>
-        // </>
-  );
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default AppRoutes
